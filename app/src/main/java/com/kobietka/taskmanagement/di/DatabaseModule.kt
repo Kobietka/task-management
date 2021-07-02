dfae452cc19d8cae8030534baacc48dde1ed88c5
kobietka <slawekgrawgry@gmail.com>
2021-07-02 12:00:41 +0200

    Add DatabaseModule class

---
+++ app/src/main/java/com/kobietka/taskmanagement/di/DatabaseModule.kt
@@ -0,0 +1,58 @@
+package com.kobietka.taskmanagement.di
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.kobietka.taskmanagement.data.Database
+import com.kobietka.taskmanagement.data.ProjectDao
+import com.kobietka.taskmanagement.data.TaskDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): Database {
+        return Room.databaseBuilder(
+            context,
+            Database::class.java,
+            "app_database"
+        ).build()
+    }
+
+    @Provides
+    fun provideTaskDao(database: Database): TaskDao {
+        return database.taskDao()
+    }
+
+    @Provides
+    fun provideProjectDao(database: Database): ProjectDao {
+        return database.projectDao()
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
