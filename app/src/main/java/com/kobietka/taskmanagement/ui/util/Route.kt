ac899523452478a1dc609e033278d9c00f922c2d
kobietka <slawekgrawgry@gmail.com>
2021-07-02 14:02:00 +0200

    Add Route class

---
+++ app/src/main/java/com/kobietka/taskmanagement/ui/util/Route.kt
@@ -0,0 +1,10 @@
+package com.kobietka.taskmanagement.ui.util
+
+
+
+class Route {
+    companion object {
+        const val main = "main"
+        const val createProject = "create_project"
+    }
+}
