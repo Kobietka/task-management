179eade5c038fe9979fd76ea04f4d1609287120d
kobietka <slawekgrawgry@gmail.com>
2021-07-02 11:59:09 +0200

    Add TaskEntity class

---
+++ app/src/main/java/com/kobietka/taskmanagement/data/TaskEntity.kt
@@ -0,0 +1,14 @@
+package com.kobietka.taskmanagement.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task")
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo val projectId: Int,
+    @ColumnInfo val name: String,
+    @ColumnInfo val description: String,
+    @ColumnInfo val creationDate: String
+)
