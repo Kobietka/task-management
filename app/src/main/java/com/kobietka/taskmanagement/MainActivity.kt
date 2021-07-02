c0c69da4265e1dd939e8c34c39919ccafe2d71d3
kobietka <slawekgrawgry@gmail.com>
2021-07-02 10:33:09 +0200

    Add dependencies

--- app/src/main/java/com/kobietka/taskmanagement/MainActivity.kt
+++ app/src/main/java/com/kobietka/taskmanagement/MainActivity.kt
@@ -28,11 +28,3 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TaskManagementTheme {
-        Greeting("Android")
-    }
-}
