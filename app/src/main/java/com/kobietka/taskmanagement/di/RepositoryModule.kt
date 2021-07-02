03565d9b14a647fbd2970da5bd31002712023f10
kobietka <slawekgrawgry@gmail.com>
2021-07-02 12:01:26 +0200

    Add RepositoryModule class

---
+++ app/src/main/java/com/kobietka/taskmanagement/di/RepositoryModule.kt
@@ -0,0 +1,47 @@
+package com.kobietka.taskmanagement.di
+
+import com.kobietka.taskmanagement.data.ProjectDao
+import com.kobietka.taskmanagement.data.TaskDao
+import com.kobietka.taskmanagement.repository.impl.ProjectRepositoryImpl
+import com.kobietka.taskmanagement.repository.impl.TaskRepositoryImpl
+import com.kobietka.taskmanagement.repository.inter.ProjectRepository
+import com.kobietka.taskmanagement.repository.inter.TaskRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideProjectRepository(projectDao: ProjectDao): ProjectRepository {
+        return ProjectRepositoryImpl(projectDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTaskRepository(taskDao: TaskDao): TaskRepository {
+        return TaskRepositoryImpl(taskDao)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
