51021527e8dba4bc372c529c5514823f6f5f096e
kobietka <slawekgrawgry@gmail.com>
2021-07-02 11:59:27 +0200

    Add ProjectEntity class

---
+++ app/src/main/java/com/kobietka/taskmanagement/data/ProjectEntity.kt
@@ -0,0 +1,12 @@
+package com.kobietka.taskmanagement.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "project")
+data class ProjectEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo val name: String,
+    @ColumnInfo val description: String
+)
