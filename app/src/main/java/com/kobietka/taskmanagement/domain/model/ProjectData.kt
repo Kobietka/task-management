b8d795b516d6786086efa1904e652d265756dd3c
kobietka <slawekgrawgry@gmail.com>
2021-07-05 11:39:43 +0200

    Add ProjectData class

---
+++ app/src/main/java/com/kobietka/taskmanagement/domain/model/ProjectData.kt
@@ -0,0 +1,5 @@
+package com.kobietka.taskmanagement.domain.model
+
+import com.kobietka.taskmanagement.data.ProjectEntity
+
+data class ProjectData(val projectEntity: ProjectEntity, val numberOfTasks: Int)
