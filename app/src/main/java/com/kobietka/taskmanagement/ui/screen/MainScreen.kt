850b1ba1d6e6d656fa95cf385304dfa24c7a385f
kobietka <slawekgrawgry@gmail.com>
2021-07-06 11:09:02 +0200

    Add status bar color change

--- app/src/main/java/com/kobietka/taskmanagement/ui/screen/MainScreen.kt
+++ app/src/main/java/com/kobietka/taskmanagement/ui/screen/MainScreen.kt
@@ -3,11 +3,13 @@ package com.kobietka.taskmanagement.ui.screen
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -20,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -28,6 +31,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.kobietka.taskmanagement.data.ProjectEntity
 import com.kobietka.taskmanagement.domain.model.ProjectData
+import com.kobietka.taskmanagement.ui.theme.orange
 import com.kobietka.taskmanagement.ui.util.Route
 
 
@@ -36,23 +40,30 @@ import com.kobietka.taskmanagement.ui.util.Route
 @Composable
 fun MainScreen(projects: State<List<ProjectData>>, navController: NavController){
     Scaffold(
+        backgroundColor = MaterialTheme.colors.primary,
         floatingActionButton = {
-            FloatingActionButton(onClick = { navController.navigate(Route.createProject) }) {
+            FloatingActionButton(backgroundColor = MaterialTheme.colors.primary, onClick = { navController.navigate(Route.createProject) }) {
                 Icon(imageVector = Icons.Filled.Add, contentDescription = "add project")
             }
         },
         topBar = {
-             Column(Modifier.padding(20.dp)) {
-                 Text(text = "Hi, \$name", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-                 Text(text = "You have got some work to do!", fontSize = 15.sp, color = Color.Gray)
-             }
+            Column(Modifier.padding(20.dp)) {
+                Text(text = "Hi, John", fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.Black)
+                Text(text = "You have got some work to do!", fontSize = 16.sp, color = Color.Black)
+            }
         },
         content = {
-            Column {
-                Text(modifier = Modifier.padding(start = 20.dp, top = 10.dp, bottom = 10.dp), text = "Your projects", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-                LazyColumn {
-                    items(projects.value.size){
-                        Project(projectData = projects.value[it], navController = navController)
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
+                    .background(MaterialTheme.colors.secondary)) {
+                Column(modifier = Modifier.padding(start = 20.dp, top = 30.dp, end = 20.dp, bottom = 20.dp)) {
+                    Text(text = "Your projects", fontSize = 19.sp, fontWeight = FontWeight.Bold, color = Color.Black)
+                    LazyColumn(modifier = Modifier.padding(top = 30.dp).fillMaxSize()) {
+                        items(projects.value.size){
+                            Project(projectData = projects.value[it], navController = navController)
+                        }
                     }
                 }
             }
@@ -65,7 +76,11 @@ fun MainScreen(projects: State<List<ProjectData>>, navController: NavController)
 @Composable
 fun Project(projectData: ProjectData, navController: NavController){
     val expanded = remember { mutableStateOf(false) }
-    Card(modifier = Modifier
+    Card(
+        elevation = 4.dp,
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier
+        .padding(bottom = 10.dp)
         .fillMaxWidth()
         .combinedClickable(
             interactionSource = remember { MutableInteractionSource() },
@@ -73,17 +88,18 @@ fun Project(projectData: ProjectData, navController: NavController){
             onClick = { navController.navigate(Route.projectDetailsRoute(projectData.projectEntity.id!!)) },
             onLongClick = { expanded.value = !expanded.value }
         )){
-        Row {
-            Column(modifier = Modifier.padding(start = 20.dp, top = 10.dp, end = 20.dp, bottom = 10.dp)) {
-                Row(modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
-                    Text(text = projectData.projectEntity.name, fontWeight = FontWeight.Medium, fontSize = 18.sp)
-                    AnimatedVisibility(visible = expanded.value) {
-                        Text(text = "${projectData.numberOfTasks} tasks")
-                    }
+
+        Row(modifier = Modifier.padding(20.dp)){
+            Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically)
+                {
+                    Text(text = projectData.projectEntity.name, fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                    Text(text = "${projectData.numberOfTasks} tasks", fontSize = 16.sp)
                 }
-                AnimatedVisibility(visible = expanded.value) {
-                    Text(text = projectData.projectEntity.description, fontSize = 16.sp, color = Color.Gray)
-                }
+                Text(modifier = Modifier.padding(top = 10.dp), text = projectData.projectEntity.description, fontSize = 18.sp)
             }
         }
     }
