9b686b77e68fd26b064b704a7e3a5bcf34342998
kobietka <slawekgrawgry@gmail.com>
2021-07-12 12:07:26 +0200

    Add foreign key to TaskEntity

--- app/src/main/java/com/kobietka/taskmanagement/data/TaskEntity.kt
+++ app/src/main/java/com/kobietka/taskmanagement/data/TaskEntity.kt
@@ -2,9 +2,10 @@ package com.kobietka.taskmanagement.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "task")
+@Entity(tableName = "task", foreignKeys = [ForeignKey(entity = ProjectEntity::class, parentColumns = arrayOf("id"), childColumns = arrayOf("projectId"))])
 data class TaskEntity(
     @PrimaryKey(autoGenerate = true) val id: Int?,
     @ColumnInfo val projectId: Int,
