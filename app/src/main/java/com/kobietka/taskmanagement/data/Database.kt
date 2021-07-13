a0c6528e87e382b9dd6395f7bb52065bd5c34577
kobietka <slawekgrawgry@gmail.com>
2021-07-13 11:18:49 +0200

    Add onDelete and onUpdate to TaskEntity

--- app/src/main/java/com/kobietka/taskmanagement/data/Database.kt
+++ app/src/main/java/com/kobietka/taskmanagement/data/Database.kt
@@ -3,7 +3,7 @@ package com.kobietka.taskmanagement.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ProjectEntity::class, TaskEntity::class, TaskStatusEntity::class], version = 4)
+@Database(entities = [ProjectEntity::class, TaskEntity::class, TaskStatusEntity::class], version = 5)
 abstract class Database : RoomDatabase() {
     abstract fun projectDao(): ProjectDao
     abstract fun taskDao(): TaskDao
--- app/src/main/java/com/kobietka/taskmanagement/data/TaskEntity.kt
+++ app/src/main/java/com/kobietka/taskmanagement/data/TaskEntity.kt
@@ -3,9 +3,21 @@ package com.kobietka.taskmanagement.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "task", foreignKeys = [ForeignKey(entity = ProjectEntity::class, parentColumns = arrayOf("id"), childColumns = arrayOf("projectId"))])
+@Entity(
+    tableName = "task",
+    foreignKeys = [
+        ForeignKey(
+            entity = ProjectEntity::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("projectId"),
+            onDelete = CASCADE,
+            onUpdate = CASCADE
+        )
+    ]
+)
 data class TaskEntity(
     @PrimaryKey(autoGenerate = true) val id: Int?,
     @ColumnInfo val projectId: Int,
