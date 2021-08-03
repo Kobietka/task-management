ea93cb342503290fd0b9e0b5880bf1f011ba4ba4
kobietka <slawekgrawgry@gmail.com>
2021-08-03 13:52:10 +0200

    Add Timer class

---
+++ app/src/main/java/com/kobietka/taskmanagement/ui/util/Timer.kt
@@ -0,0 +1,36 @@
+package com.kobietka.taskmanagement.ui.util
+
+import kotlinx.coroutines.*
+import kotlin.concurrent.timer
+
+
+class Timer {
+
+    private lateinit var tick: (Int) -> Unit
+    private var timerRunning = false
+
+    fun start(): Timer {
+        timerRunning = true
+        var timePassed = 0
+        CoroutineScope(Dispatchers.IO).launch {
+            while(timerRunning){
+                delay(1000)
+                timePassed++
+                withContext(Dispatchers.Main){
+                    tick(timePassed)
+                }
+            }
+        }
+        return this
+    }
+
+    fun stop(){
+        timerRunning = false
+    }
+
+    fun onTick(tick: (Int) -> Unit): Timer {
+        this.tick = tick
+        return this
+    }
+
+}
