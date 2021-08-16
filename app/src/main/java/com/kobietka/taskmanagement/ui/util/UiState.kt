30bfb9c79ee6db5454adda2ff7b67ede201add61
kobietka <slawekgrawgry@gmail.com>
2021-08-16 15:01:29 +0200

    Add UiState enum class

---
+++ app/src/main/java/com/kobietka/taskmanagement/ui/util/UiState.kt
@@ -0,0 +1,12 @@
+package com.kobietka.taskmanagement.ui.util
+
+enum class UiState {
+    MainScreen,
+    CreateProject,
+    ProjectDetails,
+    CreateTask,
+    TaskDetails,
+    EditProject,
+    MeasureTime
+}
+
